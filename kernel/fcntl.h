c88eab5886af78df1143b1401733e0b313601116
w83ll43 <sherillwilkins258@gmail.com>
2023-12-12 20:47:36 +0800

    lab 9

--- kernel/fcntl.h
+++ kernel/fcntl.h
@@ -1,5 +1,7 @@
-#define O_RDONLY  0x000
-#define O_WRONLY  0x001
-#define O_RDWR    0x002
-#define O_CREATE  0x200
-#define O_TRUNC   0x400
+#define O_RDONLY   0x000
+#define O_WRONLY   0x001
+#define O_RDWR     0x002
+#define O_CREATE   0x200
+#define O_TRUNC    0x400
+// 新增的标志位不能与已存在的标志位重叠
+#define O_NOFOLLOW 0x004
